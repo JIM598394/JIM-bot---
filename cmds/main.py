49c779cc3108a7b44863f6830f788c4f5673aea6
JIM580 <129885766+JIM580@users.noreply.github.com>
2024-07-06 10:41:10 +0800

    Create main.py

---
+++ cmds/main.py
@@ -0,0 +1,17 @@
+import discord
+from discord.ext import commands
+from core.classes import Cog_Extension
+
+class Main(Cog_Extension):
+    # 查看機器人延遲
+    @commands.command()
+    async def ping(self, ctx):
+        await ctx.send(f'{self.bot.latency*1000} (ms)')
+    
+    # 這個指令要在函式內部定義
+    @commands.command()      
+    async def test(self, ctx):
+        await ctx.send('123456789')
+
+def setup(bot):
+    bot.add_cog(Main(bot))
