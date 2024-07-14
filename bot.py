657036ef96f5749c10f4a15f4be8782b14712687
JIM580 <129885766+JIM580@users.noreply.github.com>
2024-07-14 10:47:14 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,19 +15,19 @@ tracemalloc.start()
 intents = discord.Intents.all()
 intents.members = True
 bot = commands.Bot(command_prefix='[',  intents=intents)
-bot.remove_command('help')
+
 
 # 確保 setting.json 存在
 if not exists("setting.json"):
     raise FileNotFoundError("不要忘記放 setting.json 或者 cd 到指定資料夾去運行 py 哦~")
 
 with open("setting.json", mode='r', encoding='utf8') as jfile:
-    jdata = json.load(jfile)
+    jdata = json.load(jfile)  
 
 # 上線狀態
 @bot.event
 async def on_ready():
-    await bot.change_presence(status=discord.Status.idle, activity=discord.Activity(type=discord.ActivityType.playing, name=""))
+    await bot.change_presence(status=discord.Status, activity=discord.Activity(type=discord.ActivityType.playing, name=""))
     print(f'✅ {bot.user.name} 已經準備好了！')
 
 
@@ -38,12 +38,12 @@ async def load(ctx, extension):
 
 @bot.command()
 async def unload(ctx, extension):
-    bot.unload_extensionload_extension(f'cmds.{extension}')
+    bot.unload_extension(f'cmds.{extension}')
     await ctx.send(f'UN-loaded{extension} done.')
 
 @bot.command()
 async def reload(ctx, extension):
-    bot.reload_extensionload_extension(f'cmds.{extension}')
+    bot.reload_extension(f'cmds.{extension}')
     await ctx.send(f'RE-loaded{extension} done.')                
 
 # 加載擴展
