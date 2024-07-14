d96492634c7e1dbdae674c7ee3def44c416d2643
JIM580 <129885766+JIM580@users.noreply.github.com>
2024-07-14 11:21:03 +0800

    Update main.py

--- cmds/main.py
+++ cmds/main.py
@@ -1,11 +1,10 @@
 import discord
 from discord.ext import commands
 from core.classes import cog_Extension
+import json
+import datetime
 
 class Main(cog_Extension):
-    
-    
-    
     # 查看機器人延遲
     @commands.command()
     async def ping(self, ctx):
